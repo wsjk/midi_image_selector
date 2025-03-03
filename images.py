cf44d17cdfd7f69453abb43533d7ff677713b6da
William Kong <william.kong@ibotta.com>
2025-03-03 15:41:30 -0700

    V0.0.0 (#3)
    * adding mask functionality
    
    * adding doc strongs
    
    * required pkgs
    
    * link to SO answer
    
    * more comments
    
    * using classes
    
    * bump

--- images.py
+++ images.py
@@ -1,3 +1,58 @@
+from image_tools import *
+
+class Images():
+    def __init__(self, path=".", mode="scroll"):
+        self.path = path
+        self.loaded_images = get_images(self.path)
+        self.curr = 0
+        self.fig, self.ax = plt.subplots()
+        self.mode = mode
+        self.display()
+        plt.get_current_fig_manager().full_screen_toggle()
+        plt.show()
+
+    def next_image(self, evt=None):
+        """
+        Load in next image
+        """
+
+        print(evt.key, type(evt.key))
+        # do something with current image
+
+        if evt.key == 'escape':
+            sys.exit()
+        try:
+            n = int(evt.key)
+            print("You pressed {}".format(n))
+        except ValueError:
+            n = self.curr
+            print("Sorry can only use number keys")
+
+        # advance to next image
+        try:
+            self.curr = n
+            self.im.set_array(self.loaded_images[self.curr])
+            self.display()
+            self.fig.canvas.draw_idle()
+        except IndexError:
+            print("Sorry no image in index: ", n)
+
+    def display(self):
+        """
+        Orchestrating function to run
+        """
+        image = self.loaded_images[self.curr]
+
+        if self.mode == "masked":
+            mask, result = get_mask(image) ## apply mask
+            self.ax.imshow(image)
+            self.ax.imshow(mask)
+            self.im = self.ax.imshow(result)
+        elif self.mode == "scroll":
+            self.im = self.ax.imshow(image)
+
+        self.fig.canvas.mpl_connect("key_press_event", self.next_image)
+=======
 ### Inspiration comes from https://stackoverflow.com/questions/53704524/show-gif-and-wait-for-key-press
 
 import matplotlib.pyplot as plt
@@ -93,9 +148,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='')
 
     parser.add_argument('--imagedir', action="store", dest='imagedir', default='')
+    parser.add_argument('--mode', action="store", dest='mode', default='scroll')
 
     args = parser.parse_args()
 
-    images = get_images(args.imagedir)
+    images = Images(args.imagedir, args.mode)
 
-    main(images)
+    images.display()
+
