e3f107b1a666fe33292d35e5062735bbdb7a5ebe
William Kong <william.kong@ibotta.com>
2025-03-03 13:05:59 -0700

    adding mask functionality

--- images.py
+++ images.py
@@ -1,17 +1,18 @@
 import matplotlib.pyplot as plt
-import glob, os
-import matplotlib.image as mpimg
+import glob, os, sys
+import cv2
+import numpy as np
 
 curr = 0
 images = []
 
-def load_images(path: str) -> list:
+def get_images(path: str) -> list:
     global images
     image_files = glob.glob(os.path.join(path, '*'))
     print(image_files)
 
     for i in image_files:
-        images.append(mpimg.imread(i))
+        images.append(cv2.imread(i))
 
     return images
 
@@ -20,6 +21,9 @@ def next_image(evt=None):
 
     print(evt.key, type(evt.key))
     # do something with current image
+
+    if evt.key == 'escape':
+        sys.exit()
 
     try:
         n = int(evt.key)
@@ -32,17 +36,35 @@ def next_image(evt=None):
     try:
         curr = n
         im.set_array(images[curr])
+        load_image(images[curr])
         fig.canvas.draw_idle()
     except IndexError:
         print("Sorry no image in index: ", curr)
 
+def get_mask(image):
+
+    mask = np.zeros(image.shape, dtype=np.uint8)
+    mask = cv2.circle(mask, (260, 300), 225, (255, 255, 255), -1)
+    result = cv2.bitwise_and(image, mask)
+    result[mask == 0] = 255
+
+    return mask, result
+
+
+def load_image(image):
+    global im, ax
+
+    mask, result = get_mask(image)
+    ax.imshow(image)
+    ax.imshow(mask)
+    im = ax.imshow(result)
+    return im
 
 def main(images):
-    global im, fig
+    global im, ax, fig
 
     fig, ax = plt.subplots()
-
-    im = ax.imshow(images[0])
+    load_image(images[0])
 
     fig.canvas.mpl_connect("key_press_event", next_image)
     plt.get_current_fig_manager().full_screen_toggle()
@@ -57,5 +79,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    images = load_images(args.imagedir)
+    images = get_images(args.imagedir)
+
     main(images)
