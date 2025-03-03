a82c59e4bc2c4780be5a470606ffdc84c4bfdaa7
William Kong <william.kong@ibotta.com>
2025-03-03 13:45:54 -0700

    V0.0.0 dev (#2)
    * adding mask functionality
    
    * adding doc strongs
    
    * required pkgs
    
    * link to SO answer
    
    * more comments

--- images.py
+++ images.py
@@ -1,25 +1,37 @@
+### Inspiration comes from https://stackoverflow.com/questions/53704524/show-gif-and-wait-for-key-press
+
 import matplotlib.pyplot as plt
-import glob, os
-import matplotlib.image as mpimg
+import glob, os, sys
+import cv2
+import numpy as np
 
 curr = 0
 images = []
 
-def load_images(path: str) -> list:
+def get_images(path: str) -> list:
+    """
+    Recursively crawl through dir and load in all images
+    """
     global images
     image_files = glob.glob(os.path.join(path, '*'))
     print(image_files)
 
     for i in image_files:
-        images.append(mpimg.imread(i))
+        images.append(cv2.imread(i))
 
     return images
 
 def next_image(evt=None):
+    """
+    Load in next image
+    """
     global curr
 
     print(evt.key, type(evt.key))
     # do something with current image
+
+    if evt.key == 'escape':
+        sys.exit()
 
     try:
         n = int(evt.key)
@@ -32,17 +44,44 @@ def next_image(evt=None):
     try:
         curr = n
         im.set_array(images[curr])
+        load_image(images[curr])
         fig.canvas.draw_idle()
     except IndexError:
         print("Sorry no image in index: ", curr)
 
+## https://stackoverflow.com/questions/59432324/how-to-mask-image-with-binary-mask
+def get_mask(image):
+    """
+    Create a mask to reveal only section of image
+    """
+    mask = np.zeros(image.shape, dtype=np.uint8)
+    mask = cv2.circle(mask, (260, 300), 225, (255, 255, 255), -1)
+    result = cv2.bitwise_and(image, mask)
+    result[mask == 0] = 255
+
+    return mask, result
+
+
+def load_image(image):
+    """
+    Load an image into window
+    """
+    global im, ax
+
+    mask, result = get_mask(image) ## apply mask
+    ax.imshow(image)
+    ax.imshow(mask)
+    im = ax.imshow(result)
+    return im
 
 def main(images):
-    global im, fig
+    """
+    Orchestrating function to run
+    """
+    global im, ax, fig
 
     fig, ax = plt.subplots()
-
-    im = ax.imshow(images[0])
+    load_image(images[0])
 
     fig.canvas.mpl_connect("key_press_event", next_image)
     plt.get_current_fig_manager().full_screen_toggle()
@@ -57,5 +96,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    images = load_images(args.imagedir)
+    images = get_images(args.imagedir)
+
     main(images)
