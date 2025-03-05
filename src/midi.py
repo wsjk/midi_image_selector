bd6fd3d27c8d3369aaa8ef4477b0fd4052f6f130
wsjk <schoolsofthought@gmail.com>
2025-03-04 20:30:18 -0700

    note values greater than num images restarts so all notes have image

--- src/midi.py
+++ src/midi.py
@@ -1,3 +1,4 @@
+import threading
 import matplotlib
 matplotlib.use('TkAgg')  # Or another suitable backend like 'QtAgg'
 import os, sys
@@ -6,40 +7,35 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 import mido
 
 from image_tools import *
+from midi_tools import *
 
-def get_midi_port():
-    # Get a list of available input port names
-    input_ports = mido.get_input_names()
-
-    # Get first port that is not a Through type
-    for p in input_ports:
-        if "Midi Through" not in p:
-            print(p)
-            port = p
-            return p
-
-    if not input_ports or not p: 
-        raise Exception("No MIDI input ports found.")
 
 class MIDI_Images():
     def __init__(
         self, 
         path=".", 
         mode="scroll", 
+        full_screen=False,
         auto_start=True
     ):
         self.path = path
         self.loaded_images = get_images(self.path)
+        self.num_images = len(self.loaded_images)
         self.curr = 0
         self.mode = mode
+        self.full_screen = full_screen
         if auto_start:
-            self.fig, self.ax = plt.subplots()
-            self.display()
-            # plt.get_current_fig_manager().full_screen_toggle()
-            self.display()
-            threading.Thread(target=self.poll_midi, daemon=1).start()
-            plt.show()  # use blocking show()
+            self.create_window()
             
+    def create_window(self):
+        self.fig, self.ax = plt.subplots()
+        self.display()
+        if self.full_screen:
+            plt.get_current_fig_manager().full_screen_toggle()
+        self.display()
+        threading.Thread(target=self.poll_midi, daemon=1).start()
+        plt.show()  # use blocking show()
+
     def poll_midi(self):
         try:
             p = get_midi_port()
@@ -49,7 +45,8 @@ class MIDI_Images():
                 while True:
                     for msg in inport.iter_pending():
                         print(msg)
-                        self.curr = msg.note - 24
+                        self.curr, self.curr_note = midi_to_note(self.num_images, msg.note)
+                        print(self.curr)
                         self.update_image()
         except Exception as e:
             print(f"Error: {e}")
@@ -59,21 +56,14 @@ class MIDI_Images():
         Load in next image
         """
 
-        # do something with current image
-        try:
-            n = int(self.curr)
-            print("You pressed {}".format(n))
-        except ValueError:
-            print("Sorry can only use MIDI notes")
-
         # advance to next image
         try:
-            self.curr = n
+            # self.curr = n
             self.im.set_array(self.loaded_images[self.curr])
             self.display()
             # self.fig.canvas.draw_idle()
         except IndexError:
-            print("Sorry no image in index: ", n)
+            print("Sorry no image in index: ", self.curr)
 
     def display(self):
             """
